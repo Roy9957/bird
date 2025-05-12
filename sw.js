39598f033949a9c8c86e1690ea9a3e30252f1a60
MOBE WORKS <ranobirroy221@gmail.com>
2025-05-12 17:01:26 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,22 @@
 self.addEventListener('push', function (event) {
-    let options = {
-        body: event.data.text(),  // The push message body
-        icon: '/icon.png',        // You can use any icon you want
-        badge: '/badge.png'       // You can use any badge image you want
+    let data = {};
+
+    try {
+        data = event.data.json();  // Expecting JSON: { title: '', body: '' }
+    } catch (e) {
+        data = {
+            title: 'MOBE GAME',
+            body: event.data.text()
+        };
+    }
+
+    const options = {
+        body: data.body || 'Join now!',
+        icon: '/icon.png',
+        badge: '/badge.png'
     };
 
     event.waitUntil(
-        self.registration.showNotification('MOBE GAME', options)
+        self.registration.showNotification(data.title || 'MOBE GAME', options)
     );
 });
